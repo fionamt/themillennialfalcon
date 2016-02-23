018e2354f72807bda6d90305798d9001c6dda794
Fiona Meyer-Teruel <fionamt@stanford.edu>
2016-02-22 21:41:37 -0800

    added all state functions, IR sensor readings

--- TheMillennialFalcon.ino
+++ TheMillennialFalcon.ino
@@ -1,5 +1,5 @@
 /*************************************************************
-  File:      FinalProject.ino
+  File:      TheMillennialFalcon.ino
   Contents:  
   Notes:     Target: Arduino Leonardo
              Arduino IDE version: 1.6.7
@@ -12,16 +12,18 @@
  
  /*---------------Includes-----------------------------------*/
 #include <Timers.h>
+#include <Servo.h>
 
  /*---------------Module Defines-----------------------------*/
  /*----- States -----*/
-#define  SEARCHING_FOR_FLAP    0
-#define  GOING_TOWARDS_FLAP    1
-#define  RELOADING_TOKENS      2
-#define  SEARCHING_FOR_BEACON  3  
-#define  GOING_TOWARDS_BEACON  4
-#define  DUMPING_TOKENS        5
-#define  SYSTEM_OFF            6
+#define  SEARCHING_FOR_FLAP        0
+#define  GOING_TOWARDS_FLAP        1
+#define  RELOADING_TOKENS          2
+#define  SEARCHING_FOR_BEACON      3  
+#define  GOING_TOWARDS_BEACON      4
+#define  DUMPING_TOKENS_SERVO_UP   5
+#define  DUMPING_TOKENS_SERVO_DOWN 6
+#define  SYSTEM_OFF                7
 
 /*----- Timers -----*/
 #define ROBOT_TIMER            0
@@ -33,15 +35,17 @@
 #define  BEACON                 1
 
 /*---------------Module Function Prototypes---*/
-void SearchingForFlap(void); // search for FLAP
+void SearchingForSensor(int sensor); // search for FLAP
 void GoingTowardsFlap(void); // return for more tokens
 void ReloadingTokens(void); // go to beacon
 void SearchingForBeacon(void); // search for beacon
 void GoingTowardsBeacon(void); // go to beacon
-void DumpingTokens(void); // initialize servo
+void DumpingTokensServoUp(void); // initialize servo
+void DumpingTokensServoDown(void); // retract servo
 void SystemOff(void);
 
 void StartTimer(int timer, int time);
+unsigned char TestTimerExpired(int timer);
 
 void TurnRight(void);
 void TurnLeft(void);
@@ -52,7 +56,7 @@ void DeployTokenDispenser(void);
 
 unsigned char TestForFLAPOrBeacon(int pin);
 unsigned char TestForCarryingTokens(void);
-unsigned char TestForIfBumperHit(void);
+unsigned char TestForBumperHit(void);
 
 
  /*---------------Global Variables-------*/
@@ -61,11 +65,15 @@ int IRPinLeft = 2; // IR pin at the left side
 int IRPinRight = 4; // IR pin at the right side
 int IRPinCenter = 7; // IR pin at the front side
 
+int servoPin = 9;
+
  /*---- State Variables-----*/
 boolean state = 0; // begin with 7 tokens
 
  /*----Other Variables-----*/
 int frequency = 0; // initialize frequency
+Servo servo;
+int servoPos = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -75,31 +83,42 @@ void setup() {
   pinMode(IRPinRight, INPUT);
   pinMode(IRPinCenter, INPUT);
   
+  // Initialize motors
+  servo.attach(servoPin);
+  
   // Start timer
   StartTimer(ROBOT_TIMER, TOTAL_TIME);  
 }
 
 void loop() {
+  if (TestTimerExpired(ROBOT_TIMER)) {
+    state = SYSTEM_OFF;
+  }
   switch(state) {
-    case(SEARCHING_FOR_FLAP): SearchingForFlap(); break;
+    case(SEARCHING_FOR_FLAP): SearchingForSensor(FLAP); break;
     case(GOING_TOWARDS_FLAP): GoingTowardsFlap(); break;
     case(RELOADING_TOKENS): ReloadingTokens(); break;
-    case(SEARCHING_FOR_BEACON): SearchingForBeacon(); break;
+    case(SEARCHING_FOR_BEACON): SearchingForSensor(BEACON); break;
     case(GOING_TOWARDS_BEACON): GoingTowardsBeacon(); break;
-    case(DUMPING_TOKENS): DumpingTokens(); break;
+    case(DUMPING_TOKENS_SERVO_UP): DumpingTokensServoUp(); break;
+    case(DUMPING_TOKENS_SERVO_DOWN): DumpingTokensServoDown(); break;
     case(SYSTEM_OFF): SystemOff(); break;    
     
     default: Serial.println("Something went horribly wrong");
   }
 }
 
-void SearchingForFlap(void) {
-  if (TestForFLAPOrBeacon(IRPinCenter) == FLAP) {
+void SearchingForSensor(int sensor) {
+  if (TestForFLAPOrBeacon(IRPinCenter) == sensor) {
     Stop();
-    state = GOING_TOWARDS_FLAP;
-  } else if (TestForFLAPOrBeacon(IRPinRight) == FLAP) {
+    if (sensor == FLAP) {
+      state = GOING_TOWARDS_FLAP;
+    } else {
+      state = GOING_TOWARDS_BEACON;
+    }
+  } else if (TestForFLAPOrBeacon(IRPinRight) == sensor) {
     TurnRight();
-  } else if (TestForFLAPOrBeacon(IRPinLeft) == FLAP) {
+  } else if (TestForFLAPOrBeacon(IRPinLeft) == sensor) {
     TurnLeft();
   } else {
     TurnRight();
@@ -107,8 +126,9 @@ void SearchingForFlap(void) {
 }
 
 void GoingTowardsFlap(void) {
-  if (TestForIfBumperHit()) {
+  if (TestForBumperHit()) {
     Stop();
+    state = RELOADING_TOKENS;
   } else if (TestForFLAPOrBeacon(IRPinCenter) == FLAP) {
      GoForward();
    } else {
@@ -116,15 +136,57 @@ void GoingTowardsFlap(void) {
    }
 }
 
-void ReloadingTokens(void); // go to beacon
-void SearchingForBeacon(void); // search for beacon
-void GoingTowardsBeacon(void); // go to beacon
-void DumpingTokens(void) {
+void ReloadingTokens(void) {
+  if (TestForCarryingTokens()) {
+    state = SEARCHING_FOR_BEACON; 
+  } else {
+    Stop();
+  }
 }
-void SystemOff(void);
+
+void GoingTowardsBeacon(void) {
+  if (TestForBumperHit()) {
+    Stop();
+    state = DUMPING_TOKENS_SERVO_UP;
+  } else if (TestForFLAPOrBeacon(IRPinCenter) == BEACON) {
+     GoForward();
+   } else {
+      state = SEARCHING_FOR_BEACON; 
+   }
+}
+
+void DumpingTokensServoUp(void) {
+  if (!TestForCarryingTokens()) {
+    state = SEARCHING_FOR_FLAP;
+  } else {
+    servoPos = 180;
+    servo.write(servoPos);
+    delay(15);
+    state = DUMPING_TOKENS_SERVO_DOWN;
+  }
+}
+
+void DumpingTokensServoDown(void) {
+  if (!TestForCarryingTokens()) {
+    state = SEARCHING_FOR_FLAP;
+  } else {
+    servoPos = 0;
+    servo.write(servoPos);
+    delay(15);
+    state = DUMPING_TOKENS_SERVO_UP;
+  }
+}
+
+void SystemOff(void) {
+ Stop(); 
+}
 
 void StartTimer(int timer, int time) {
   TMRArd_InitTimer(timer, time);  
+}
+
+unsigned char TestTimerExpired(int timer) {
+  return (unsigned char)(TMRArd_IsTimerExpired(timer));
 }
 
 unsigned char TestForFLAPOrBeacon(int pin) {
@@ -137,4 +199,32 @@ unsigned char TestForFLAPOrBeacon(int pin) {
     Serial.println("I see a balance!");
     return BEACON;
   }
+}
+
+unsigned char TestForCarryingTokens(void) {
+  
+}
+
+unsigned char TestForBumperHit(void) {
+  
+}
+
+void TurnRight(void) {
+  Serial.println("Turning Right!");
+}
+
+void TurnLeft(void) {
+  Serial.println("Turning Left!");
+}
+
+void GoForward(void) {
+  Serial.println("Go Forward!");
+}
+
+void GoBackwards(void) {
+  Serial.println("Go Backwards!");
+}
+
+void Stop(void) {
+    Serial.println("Turn off motors!");
 }
